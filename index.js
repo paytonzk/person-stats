c726d14280d21892bed44685443b59c957ab4064
Zach Payton <paytonzk@rose-hulman.edu>
2017-06-26 19:39:10 -0700

    Finished all homework opportunities

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ function changeHeading(ev) {
   ev.preventDefault()
   const f = ev.target
   const name = f.personName.value
-  const heading = document.querySelector('h1')
-  heading.innerHTML = name
+  const p = document.querySelector('#stats')
+  p.innerHTML += name
+}
+
+function changeParagraph(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const name = f.colorName.value
+  const p = document.querySelector('#stats')
+  p.style.color = name
 }
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
+const colorForm = document.querySelector('#color-form')
+colorForm.addEventListener('submit', changeParagraph)
