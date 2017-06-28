aa6ebae3b1f719bb356ea3d9359f866a85e48cf1
Zach Payton <paytonzk@rose-hulman.edu>
2017-06-27 17:36:01 -0700

    Catching up with class

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-function changeHeading(ev) {
+function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target
   const name = f.personName.value
-  const p = document.querySelector('#stats')
-  p.innerHTML += name
+  const age = f.personAge.value
+  const color = f.favoriteColor.value
+
+  const div = document.querySelector('#stats')
+  
+  const list = document.createElement('ul')
+
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${name}`
+  list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = 'Favorite Color: '
+ 
+  colorItem.appendChild(renderColor(color))
+
+  list.appendChild(colorItem)
+
+  div.appendChild(list)
+
 }
 
-function changeParagraph(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const name = f.colorName.value
-  const p = document.querySelector('#stats')
-  p.style.color = name
+function renderColor(color){
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  return colorDiv
 }
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeHeading)
-const colorForm = document.querySelector('#color-form')
-colorForm.addEventListener('submit', changeParagraph)
+personForm.addEventListener('submit', handleSubmit)
